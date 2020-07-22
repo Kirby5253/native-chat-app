b21ef13eb42c38bc89efbd52be6fd2df7e0bf74e
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-21 21:15:12 -0500

    Implemented chat component

--- Components/Chat.js
+++ Components/Chat.js
@@ -1,9 +1,67 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat';
+// Only needed if the keyboard is covering input
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
 //The apps chat component which renders a chat interface where users select a friend and chat with them
 export default class Chat extends React.Component {
+	state = {
+		messages: []
+	};
+
+	componentDidMount() {
+		let name = this.props.route.params.name;
+		this.setState({
+			messages: [
+				{
+					_id: 1,
+					text: 'Hello developer',
+					createdAt: new Date(),
+					user: {
+						_id: 2,
+						name: 'React Native',
+						avatar: 'https://placeimg.com/140/140/any',
+					},
+				},
+				{
+					_id: 2,
+					text: name +` has entered the chat.`,
+					createdAt: new Date(),
+					system: true,
+				}
+			]
+		});
+	}
+
+	// This appends the message to the state of the chat
+	onSend = (messages = []) => {
+		this.setState((previousState) => ({
+			messages: GiftedChat.append(previousState.messages, messages)
+		}));
+	};
+
+	// Allows customization of chat bubble color
+	renderBubble(props) {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					right: {
+						backgroundColor: '#000'
+					}
+				}}
+			/>
+		)
+	}
+
+	// Custom send button color 
+	renderSend(props) {
+		return (
+			<Send {...props} textStyle={{ color: 'black'}} label={'Send'} />
+		);
+	}
+
 	render() {
 		let name = this.props.route.params.name;
 		let background = this.props.route.params.background;
@@ -15,12 +73,24 @@ export default class Chat extends React.Component {
 			<View
 				style={{
 					flex: 1,
-					justifyContent: 'center',
-					alignItems: 'center',
 					backgroundColor: background
 				}}
 			>
-				<Text style={{ color: '#fff' }}>Hello {name}!</Text>
+				<GiftedChat
+					// Allows for black message text for the light background option 
+					textStyle={background === "#B9C6AE" || background === "#8A95A5" ? {color: 'black'} : {color: '#CFD1D2'}}
+					// Changes color of the bubble
+					// renderBubble={this.renderBubble.bind(this)}
+					renderSend={this.renderSend.bind(this)}
+					messages={this.state.messages}
+					onSend={(messages) => this.onSend(messages)}
+					user={{
+						_id: 1
+					}}
+				/>
+				<Send textStyle={{ color: 'red' }} label={'Send'} />
+				{/* This currently is not necessary for the current android, it actually breaks the app */}
+				{/* {Platform.OS === 'android' ? <KeyboardSpacer /> : null } */}
 			</View>
 		);
 	}
